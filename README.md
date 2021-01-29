71654cd7353b5648bc90a180b2bc1115074d8f35
Felipe <fesanmar@gmail.com>
2021-01-29 18:09:44 +0100

    Adds user input validation and fixes a little but

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # tleft
-Super naive program written in Haskell to subtract two timestamps with the format `hh: mm`. 
+
+Super naive CLI aplication that subtracts two timestamps with the format `hh:mm`.
 
 ## Usage
-Compile it with `ghc tleft.hs` and run `tleft <timestamp1> <timestamp2>`. If it's executed with a different timestamp format than allowed or with 
-fewer arguments than two, it will print a non-customized error message. Otherwise, a message with the format `Time left: <hh>:<mm> hours` will be displayed on the screen.
+
+Compile it with `ghc tleft.hs` and run `tleft <timestamp1> <timestamp2>`. A message with the format `Time left: <hh>:<mm> hours` will be displayed on the screen.
 
 ## Example
-```
+
+```CMD
 $ tleft 20:12 17:15
-Time left: 2:57 hours
+Time left: 02:57 hours
 ```
--- tleft.hs
+++ tleft.hs
@@ -1,9 +1,77 @@
+{-|
+Module      : tleft
+Description : Super naive CLI aplication that subtracts two timestamps with the format hh:mm.
+Copyright   : (c) Felipe-Cruz Martínez-Acalá, 2021
+License     : MIT license
+Maintainer  : fesanmar@gmail.com
+Stability   : experimental
+-}
+
 import System.Environment
+import Text.Read
     
 main :: IO ()
 main = do
     args <- getArgs 
-    putStrLn (timeLeftFromArgs (head args) (args !! 1))
+    argParser args
+
+usage = "Usage: \n\t tleft <hh:mm> <hh:mm>"
+
+argParser :: [String] -> IO ()
+argParser (x:[])
+    | x `elem` helpCommands = putStrLn usage
+    | otherwise = putStrLn ("Error: Must specify two timestamps.\n" ++ usage)
+    where helpCommands = ["--help", "-h"]
+argParser (x:y:[])
+    | isValidTimeRange x y = putStrLn (timeLeftFromArgs x y)
+    | areTimestamps [x, y] = putStrLn ("Error: first arg <" ++ x ++ ">" ++ " must be greater or equal to the second <" ++ y ++ ">\n" ++ usage)
+argParser args@(x:y:zs) = putStrLn ("Error: This program expects only two args but " ++ (show (length args)) ++ " where passsed.\n" ++ usage)
+
+isValidTimeRange :: String -> String -> Bool
+isValidTimeRange a b = areTimestamps [a, b] && a `isBiggerThanOrEqualTo` b
+
+isBiggerThanOrEqualTo :: String -> String -> Bool
+isBiggerThanOrEqualTo a b
+    | h > h' = True
+    | h == h' = m > m' || m == m'
+    | otherwise = False
+    where 
+        h = fromArgToHours a
+        h' = fromArgToHours b
+        m = fromArgToMinutes a
+        m' = fromArgToMinutes b
+
+areTimestamps :: [String] -> Bool
+areTimestamps (x:[]) = isValidTime x
+areTimestamps (x:xs) = isValidTime x && areTimestamps xs
+
+isValidTime :: String -> Bool
+isValidTime (h:h':colon:m:m':[])
+    | isValidTime' [h, h'] [colon] [m, m'] = True
+    | otherwise = False
+isValidTime (h:colon:m:m':[])
+    | isValidTime' [h] [colon] [m, m'] = True
+    | isValidTime' [h] [colon] [m] = True
+    | otherwise = False
+isValidTime (h:colon:m:m':_) = False
+
+isValidTime' :: String -> String -> String -> Bool
+isValidTime' h colon m = isValidHour h && colon == ":" && isValidMinute m
+
+isValidHour :: String -> Bool
+isValidHour a
+    | isInt a = castedString >= 0
+    | otherwise = False 
+    where castedString = (read a :: Int)
+
+isValidMinute :: String -> Bool
+isValidMinute a
+    | isInt a = castedString >= 0  && castedString <= 59 
+    | otherwise = False 
+    where castedString = (read a :: Int)
+
+isInt :: String -> Bool
+isInt a = (readMaybe a :: Maybe Int) /= Nothing
 
 timeLeftFromArgs :: [Char] -> [Char] -> [Char]
 timeLeftFromArgs arg1 arg2 = timeLeftMessage (fromArgToTimeTuple arg1) (fromArgToTimeTuple arg2)
@@ -12,20 +80,31 @@ fromArgToTimeTuple :: [Char] -> (Int, Int)
 fromArgToTimeTuple arg = (fromArgToHours arg, fromArgToMinutes arg)
 
 fromArgToHours :: [Char] -> Int
-fromArgToHours arg = read (take 2 arg)
+fromArgToHours (x:_:_:_:[]) = read [x]
+fromArgToHours (x:y:_:__:_:[]) = read [x, y]
 
 fromArgToMinutes :: [Char] -> Int
-fromArgToMinutes arg = read (take 2 (drop 3 arg))
+fromArgToMinutes (_:_:_:m:m':[]) = read ([m, m'])
+fromArgToMinutes (_:_:m:m':[]) = read ([m, m'])
 
 timeLeftMessage :: (Int, Int) -> (Int, Int) -> [Char]
 timeLeftMessage time1 time2 = composeTimeLeftString (timeLeft time1 time2)
 
 composeTimeLeftString :: (Int, Int) -> [Char]
-composeTimeLeftString time = "Time left -> " ++ show (fst time) ++ ":" ++ show (snd time) ++ " hours"
+composeTimeLeftString time = "Time left -> " ++ twoDigitFormatt (fst time) ++ ":" ++ twoDigitFormatt (snd time) ++ " hours"
+
+twoDigitFormatt :: Int -> String
+twoDigitFormatt n
+    | n >= 10 || n < 0 = show n
+    | otherwise = "0" ++ (show n)
 
 timeLeft :: (Int, Int) -> (Int, Int) -> (Int, Int)
-timeLeft time1 time2 = (hours, subMinutes (snd time1) (snd time2)) where 
-    hours = if snd time1 > snd time2 then fst time1 - fst time2 else fst time1 - fst time2 - 1
+timeLeft time1 time2 = (hours, minutes) where 
+    minutes = let minutes' = subMinutes (snd time1) (snd time2) in
+        if minutes' < 60 then minutes' else 00
+    hours = if snd time1 >= snd time2 
+            then fst time1 - fst time2 
+            else fst time1 - fst time2 - 1
 
 subMinutes :: Int -> Int -> Int 
 subMinutes m1 m2 = if m1 > m2 then m1 - m2 else m1 - m2 + 60
